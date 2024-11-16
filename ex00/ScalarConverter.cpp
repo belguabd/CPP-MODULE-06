b471942cd5bac6e7ef008355f02fadd9842f2b85
belguabd <brahim2001elguabd@gmail.com>
2024-11-16 17:11:52 +0100

    exercise ex00 is finished

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belguabd <belguabd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 16:16:20 by belguabd          #+#    #+#             */
+/*   Updated: 2024/11/16 16:17:58 by belguabd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+#include <sstream>
+#include <cmath>
+
+static int parse_input(const std::string &data)
+{
+    if (data.size() > 1 && std::isalpha(data[0]))
+        std::cout << "Error in your input!!\n", std::exit(1);
+
+    for (size_t i = 1; i < data[i]; i++)
+        if (std::isalpha(data[i]) && data[i] != 'f')
+            std::cout << "Error in your input!!\n", std::exit(1);
+    return (0);
+}
+static long double ft_atoi(const std::string &data)
+{
+    long double num;
+
+    std::size_t pos = data.find('f', 0);
+    if (pos != std::string::npos)
+        std::stringstream(data.substr(0, pos)) >> num;
+    else
+        std::stringstream(data) >> num;
+    return num;
+}
+
+void ScalarConverter::convert(const std::string &data)
+{
+    parse_input(data);
+    long double num = ft_atoi(data);
+    if (std::isalpha(data[0]))
+        num = data[0];
+    char char_value = static_cast<char>(num);
+    int int_value = static_cast<int>(num);
+    double double_value = static_cast<double>(num);
+    float flaot_value = static_cast<float>(num);
+
+    if (std::isprint(char_value))
+        std::cout << "char: " << char_value << std::endl;
+    else
+        std::cout << "char: " << "Non displayable" << std::endl;
+    
+    std::cout << "int: " << int_value << std::endl;
+    std::cout << "float: " << flaot_value << (std::floor(num) == num ? ".0f" : "f") << std::endl;
+    std::cout << "double: " << double_value << (std::floor(num) == num ? ".0" : "") << std::endl;
+};
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belguabd <belguabd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 16:14:51 by belguabd          #+#    #+#             */
+/*   Updated: 2024/11/16 14:34:25 by belguabd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class ScalarConverter
+{
+public:
+    static void convert( const std::string &data);
+
+
+};
